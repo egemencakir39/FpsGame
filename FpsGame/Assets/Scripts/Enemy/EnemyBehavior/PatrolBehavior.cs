fdf016d20b2b423cfa91e3d8f5afa2cc6ac37f86
egemencakir39 <egemencakir29@gmail.com>
2024-02-17 13:43:41 +0300

    patrol enemy fix

--- FpsGame/Assets/Scripts/Enemy/EnemyBehavior/PatrolBehavior.cs
+++ FpsGame/Assets/Scripts/Enemy/EnemyBehavior/PatrolBehavior.cs
@@ -1,19 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PatrolBehavior : StateMachineBehaviour
 {
     float timer;
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    List<Transform> wayPoints = new List<Transform>();
+    NavMeshAgent agent;
+    
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         timer = 0;
+        Transform wayPointsObject = GameObject.FindGameObjectWithTag("WayPoints").transform;
+        foreach (Transform t in wayPointsObject)
+        {
+            wayPoints.Add(t);
+        }
+
+        if (wayPoints.Count > 0)
+        {
+            agent = animator.GetComponent<NavMeshAgent>();
+            agent.SetDestination(wayPoints[0].position);
+        }
+        else
+        {
+            Debug.LogError("No waypoints found!");
+        }
     }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            agent.SetDestination(wayPoints[Random.Range(0, wayPoints.Count)].position);
+        }
         timer += Time.deltaTime;
         if (timer > 10)
         {
@@ -21,21 +42,12 @@ public class PatrolBehavior : StateMachineBehaviour
         }
     }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+   
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        agent.SetDestination(agent.transform.position);
     }
 
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+    
+    
 }
